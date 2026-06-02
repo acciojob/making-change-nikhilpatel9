fa4ee90bafa5bad7ff32a7305335d1387ef77b76
NIKHIL PATEL <115173157+nikhilpatel9@users.noreply.github.com>
2026-06-02 14:35:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const makeChange = (c) => {
   // your name here
+	let a=c/25,b=(c-a)/10,l=(c-a-b)/10,e=(c-a-b-l);
+	let result = {"q":a,"d":b,"n":l,"p":e};
+	return result;
 };
 
 // Do not the change the code below
